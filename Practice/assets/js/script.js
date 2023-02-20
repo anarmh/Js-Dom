14e401ff22e46363bc53519679db517e595812f0
anarmh <anarmh@code.edu.az>
2023-02-20 08:00:16 -0800

    add classname of js

--- Practice/assets/js/script.js
+++ Practice/assets/js/script.js
@@ -22,11 +22,27 @@
 // let thirdspan=document.querySelector("#product span:nth-child(3)");
 // console.log(thirdspan);
 
-let spans = document.querySelectorAll("#product span");
+// let spans = document.querySelectorAll("#product span");
 
 // console.log(spans);
-for (const item of spans) {
-  if (item.innerText == "Salam3") 
-    item.style.color = "red";
+// for (const item of spans) {
+//   if (item.innerText == "Salam3") 
+//     item.style.color = "red";
   
-}
+// }
+
+// let elem=document.querySelector("h2");
+// elem.innerText="Welcome";
+// console.log(elem);
+// console.log(elem.innerHTML);
+
+// elem.innerHTML="<span>Salam</span>"
+// console.log(elem);
+// elem.style.backgroundColor="green";
+// elem.style.position="relative";
+// elem.style.top="300px"
+
+let h1=document.querySelector("h1");
+
+h1.className="test";
+console.log(h1);
